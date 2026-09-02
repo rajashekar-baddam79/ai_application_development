d926407b4b7705ef15ee60d82d1a2c2a04feb2e5
RAJASHEKAR <baddamrajashekar27@gmail.com>
2026-09-02 14:55:55 +0530

    python variables completed

---
+++ ai-python-foundations/day01/variables.py
@@ -0,0 +1,10 @@
+#Dynamic Typing
+#int, float, bool, str
+name="Geela"
+age = 25
+weight = 49.85
+is_present = True
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(is_present))
